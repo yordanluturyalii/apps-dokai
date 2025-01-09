0bc826c93b81db035b7b9dd026512b0e09638a3c
Abdul Aziz <abdulazizmishabhudin@gmail.com>
2025-01-09 17:39:06 +0700

    chore: update card :lipstick:

--- app/ask-ai/(tabs)/care-connect.tsx
+++ app/ask-ai/(tabs)/care-connect.tsx
@@ -1,23 +1,61 @@
 import Card from "@/components/Card";
 import { Drug } from "@/constants";
-import { Text, View } from "react-native";
+import { useApi } from "@/hooks/useApi";
+import { ScrollView, Text, View } from "react-native";
+
+interface CareConnectScreenResponse {
+	messaage: string;
+	data: {
+		id: number;
+		brand_name: string;
+		name: string;
+		price: string;
+		image_url: string;
+	};
+}
 
 export default function CareConnectScreen() {
+	const { data, error, isLoading, fetchData } =
+		useApi<CareConnectScreenResponse>();
+
+	const getData = async () => {
+		await fetchData({
+			method: "GET",
+			uri: "/complaints/c118559b-b734-43fa-a0de-4b6943976333/recommendations",
+			data: {},
+		});
+	};
+
+	const createRows = (items: typeof Drug) => {
+		const rows: (typeof Drug)[] = [];
+		for (let i = 0; i < items.length; i += 2) {
+			rows.push(items.slice(i, i + 2));
+		}
+		return rows;
+	};
+
 	return (
-		<>
-			<Text className="title-30 text-grayscale-text-title">
+		<ScrollView className="p-4">
+			<Text className="pb-4 title-30 text-grayscale-text-title">
 				Drug recommendations
 			</Text>
-			{Drug.map((card) => {
-				return (
-					<Card
-						category={card.category}
-						item={card.item}
-						price={card.price}
-						key={card.id}
-					/>
-				);
-			})}
-		</>
+
+			<View className="flex-col gap-y-4">
+				{createRows(Drug).map((row, rowIndex) => (
+					<View key={`row-${rowIndex}`} className="flex-row justify-between">
+						{row.map((card) => (
+							<Card
+								key={card.id}
+								brand_name={card.brand_name}
+								item={card.item}
+								price={card.price}
+								className="w-[48%]"
+							/>
+						))}
+						{row.length === 1 && <View className="w-[48%]" />}
+					</View>
+				))}
+			</View>
+		</ScrollView>
 	);
 }
--- components/Card.tsx
+++ components/Card.tsx
@@ -2,16 +2,22 @@ import { Image, Text, View } from "react-native";
 import ThemedButton from "./ThemedButton";
 
 export default function Card({
-	category,
+	brand_name,
 	price,
 	item,
-}: { category: string; price: string; item: string }) {
+	className,
+}: { brand_name: string; price: string; item: string; className?: string }) {
 	return (
-		<View className="p-3 rounded-3xl bg-grayscale-surface-default-subtle ">
-			<Image source={require("@/assets/images/dokai-icon.svg")} />
+		<View
+			className={`p-3 rounded-3xl bg-grayscale-surface-default-subtle ${className}`}
+		>
+			<Image
+				source={require("@/assets/images/dokai-icon.svg")}
+				className="object-contain w-36 h-36"
+			/>
 			<View className="flex-row justify-between w-full">
 				<Text className="text-grayscale-text-caption subtitle-10">
-					{category}
+					{brand_name}
 				</Text>
 				<Text className="title-20 text-grayscale-text-title">{price}</Text>
 			</View>
