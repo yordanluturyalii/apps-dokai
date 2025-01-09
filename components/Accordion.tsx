c74d30dc5ee842faf078e1f893a583000d2d6eef
Abdul Aziz <abdulazizmishabhudin@gmail.com>
2025-01-09 17:02:45 +0700

    feat: add function to component Accordion :sparkles:

--- components/Accordion.tsx
+++ components/Accordion.tsx
@@ -1,21 +1,60 @@
 import ArrowDown from "@/assets/images/icons/arrow-down.svg";
-import { Text, TouchableOpacity, View } from "react-native";
+import { useState } from "react";
+import {
+	Animated,
+	LayoutAnimation,
+	Text,
+	TouchableOpacity,
+	View,
+} from "react-native";
+
+type AccordionProps = {
+	title: string;
+	description: string;
+	initiallyExpanded?: boolean;
+};
 
 export default function Accordion({
 	title,
 	description,
-}: { title: string; description: string }) {
+	initiallyExpanded = true,
+}: AccordionProps) {
+	const [isExpanded, setIsExpanded] = useState(initiallyExpanded);
+	const [rotateAnimation] = useState(new Animated.Value(0));
+
+	const toggleAccordion = () => {
+		LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+
+		Animated.timing(rotateAnimation, {
+			toValue: isExpanded ? 0 : 1,
+			duration: 200,
+			useNativeDriver: true,
+		}).start();
+
+		setIsExpanded(!isExpanded);
+	};
+
+	const arrowRotation = rotateAnimation.interpolate({
+		inputRange: [0, 1],
+		outputRange: ["0deg", "180deg"],
+	});
+
 	return (
 		<View className="p-5 bg-primary-surface-default-subtle rounded-3xl">
-			<View className="flex-row items-center justify-between">
+			<TouchableOpacity
+				onPress={toggleAccordion}
+				className="flex-row items-center justify-between"
+			>
 				<Text className="text-grayscale-text-title link-30">{title}</Text>
-				<TouchableOpacity onPress={() => {}}>
+				<Animated.View style={{ transform: [{ rotate: arrowRotation }] }}>
 					<ArrowDown width={20} height={20} />
-				</TouchableOpacity>
-			</View>
-			<Text className="pt-3 body-10 text-grayscale-text-body">
-				{description}
-			</Text>
+				</Animated.View>
+			</TouchableOpacity>
+			{isExpanded && (
+				<Text className="pt-3 body-10 text-grayscale-text-body">
+					{description}
+				</Text>
+			)}
 		</View>
 	);
 }
