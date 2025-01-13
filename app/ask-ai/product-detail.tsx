3f3a3b7b8ed2401481548d416b083bbfb00a69d0
yordanluturyalii <yordanluturyali@gmail.com>
2025-01-13 22:32:14 +0700

    feat: integrating detail product with api :sparkles:

--- app/ask-ai/product-detail.tsx
+++ app/ask-ai/product-detail.tsx
@@ -1,12 +1,44 @@
 import HeaderBack from "@/components/HeaderBack";
 import ThemedButton from "@/components/ThemedButton";
+import { useApi } from "@/hooks/useApi";
+import { useAuth } from "@/hooks/useAuth";
 import { useRouter } from "expo-router";
 import { useLocalSearchParams } from "expo-router";
+import { useEffect } from "react";
 import { Image, Linking, Text, View } from "react-native";
+
+interface ProductDetailProps {
+	id: number;
+	brand_name: string;
+	name: string;
+	description: string;
+	price: number;
+	image_url: string;
+}
 
 export default function ProductDetail() {
 	const route = useRouter();
-	const { id, price, brand_name, item } = useLocalSearchParams();
+	const { id } = useLocalSearchParams();
+	const { data, error, isLoading, fetchData } = useApi<ProductDetailProps>();
+	const {token} = useAuth();
+
+	useEffect(() => {
+		getProductDetail();
+	}, [id]);
+
+	const getProductDetail = async () => {
+		await fetchData({
+			uri: `/drugs/${id}`,
+			method: "GET",
+			headers: {
+				Authorization: token,
+			}
+		});
+
+		if (error) {
+			console.log(error);
+		}
+	}
 
 	return (
 		<View className="flex-1">
@@ -21,20 +53,17 @@ export default function ProductDetail() {
 			<View className="flex-row justify-between">
 				<View className="flex-1">
 					<Text className="text-grayscale-text-caption subtitle-20">
-						Brand Name
+						{data?.data.brand_name}
 					</Text>
-					<Text className="title-30">Product Name</Text>
+					<Text className="title-30">{data?.data.name}</Text>
 				</View>
 				<Text className="flex-1 flex-shrink text-right display-60 text-grayscale-text-title">
-					Price
+					Rp {data?.data.price}
 				</Text>
 			</View>
 			<Text className="pt-6 pb-3 title-20">Description</Text>
 			<Text className="flex-1 body-10">
-				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim
-				reiciendis illo rerum quia autem repudiandae quae laudantium, beatae,
-				dolorum veniam perspiciatis expedita? Asperiores, voluptas repudiandae.
-				Iure, laboriosam dolores? Veritatis, delectus.
+				{data?.data.description}
 			</Text>
 			<ThemedButton
 				onPress={() =>
